da341320754f97a7c9f696cae8a0a448d80e6062
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-10 14:13:56 -0500

    Change App presentational component to Container Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Container } from 'reactstrap'
 import SignIn from './Components/SignIn'
 
-function App() {
-  return (
-    <Container className="App">
-    <SignIn />
-  </Container>
-  );
+class App extends Component {
+  constructor(props){
+    super()
+  }
+  render(){
+    return (
+      <Container className="App">
+      <SignIn />
+    </Container>
+    )
+  }
 }
 
 export default App;
