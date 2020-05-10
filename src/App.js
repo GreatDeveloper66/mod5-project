64884f4917151cf615946a48ef4d97282a7b45be
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-10 13:28:16 -0500

    Add row and column structure to login form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import './App.css';
-import { Container, FormGroup, Form, Col, Label, Input, Button } from 'reactstrap'
+import { Container, FormGroup, Form, Col, Label, Input, Button, Row } from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
-    <Container>
-        <h2>Sign In</h2>
-        <Form className="form">
-          <Col>
-            <FormGroup>
-              <Label>UserName</Label>
-              <Input
-                type="email"
-                name="email"
-                id="exampleEmail"
-                placeholder="myemail@email.com"
-              />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="examplePassword"
-                placeholder="********"
-              />
-            </FormGroup>
-          </Col>
-          <Button>Submit</Button>
-        </Form>
-      </Container>
-    </div>
+    <Container className="App">
+    <Row>
+      <Col>
+      </Col>
+      <Col>
+      <h2>Sign In</h2>
+      <Form className="form">
+        <Col>
+          <FormGroup>
+            <Label>UserName</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="myemail@email.com"
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="********"
+            />
+          </FormGroup>
+        </Col>
+        <Button>Submit</Button>
+      </Form>
+      </Col>
+      <Col>
+      </Col>
+    </Row>
+  </Container>
   );
 }
 
