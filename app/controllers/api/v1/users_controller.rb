ecec5b8ea6fbcbd71da00502d449aa52224d88c2
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-12 13:06:26 -0500

    Set up auth controller for user login and profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-    render json: User.all
-  end
-  def create
-       @user = User.create(user_params)
-       if(@user.valid?)
-         render json: {userObj: UserSerializer.new(@user)}
-       else
-         render json: {error: 'user creation failed'}
-       end
-   end
+  skip_before_action :authorized, only: [:create,:index]
 
-  private
-     def user_params
-       params.require(:user).permit(:email, :username, :password)
-     end
+    def index
+      render json: User.all
+    end
 
+    def profile
+      render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token({ user_id: @user.id })
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :bio, :avatar)
+    end
 end
