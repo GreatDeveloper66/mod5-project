a1a664f4a8a24c32a95d4cb97cc5a3f6e1b2a1ca
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-12 13:37:59 -0500

    Add SignInUser mthod to SignIn Component

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -14,11 +14,35 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-const SignIn = props =>
+const SignIn = props => {
+
+const SignInUser = event => {
+  event.preventDefault()
+  const username = event.target.username.value
+  const password = event.target.password.value
+  const userObj = {
+    user: {
+      username: username,
+      password: password
+    }
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userObj)
+  }
+  fetch('http://localhost:5000/api/v1/login', configObj)
+    .then(resp => resp.json())
+    .then(data => console.log('login', data))
+}
+return (
 <Row className="d-flex justify-content-center">
   <Col xs={12} sm={8} lg={4}>
   <h2>Sign In</h2>
-  <Form className="form">
+  <Form className="form" onSubmit={SignInUser}>
     <Col>
       <FormGroup>
         <Label>Username</Label>
@@ -49,5 +73,6 @@ const SignIn = props =>
   </Form>
   </Col>
 </Row>
-
+)
+}
 export default connect(null,mapDispatchToProps)(SignIn)
