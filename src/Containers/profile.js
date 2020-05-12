1be508b2fe42f676a3dd05aa538a1cf9ac7b55bf
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-12 16:29:38 -0500

    Add HandleSubmit Method to Profile Form

--- src/Containers/profile.js
+++ src/Containers/profile.js
@@ -1,50 +1,29 @@
 import React from 'react';
 import '../App.css';
 import { FormGroup, Form, Col, Label, Input, Button, Row } from 'reactstrap'
+import BottomForm from '../Components/bottomform'
+import TopForm from '../Components/topform'
 
-const Profile = () => <Row className="d-flex justify-content-center">
-  <Col xs={12} sm={8} lg={4}>
-  <h2>Profile</h2>
-  <Form className="form">
-    <Col>
-      <FormGroup>
-        <Label>Email</Label>
-        <Input
-          type="email"
-          name="email"
-          id="exampleEmail"
-          placeholder="myemail@email.com"
-        />
-      </FormGroup>
-    </Col>
-    <Col>
-      <FormGroup>
-        <Label>Username</Label>
-        <Input
-          type="text"
-          name="username"
-          id="exampleusername"
-          placeholder="username"
-        />
-      </FormGroup>
-    </Col>
-    <Col>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="examplePassword"
-          placeholder="********"
-        />
-      </FormGroup>
-    </Col>
-    <Col className="d-flex justify-content-around">
-      <Button>Delete</Button>
-      <Button type="submit">Save</Button>
-      </Col>
-  </Form>
-  </Col>
-</Row>
+const Profile = () => {
+                      const handleSubmit = event => {
+                        event.preventDefault()
+                        console.log(event.target)
+                      }
+                      return (
+                        <Row className="d-flex justify-content-center">
+                          <Col xs={12} sm={8} lg={4}>
+                          <h2>Profile</h2>
+                          <Form className="form" onSubmit={handleSubmit}>
+                            <TopForm />
+                            <BottomForm />
+                            <Col className="d-flex justify-content-around">
+                              <Button>Delete</Button>
+                              <Button type="submit">Save</Button>
+                              </Col>
+                          </Form>
+                          </Col>
+                        </Row>
+                    )
+                    }
 
 export default Profile
