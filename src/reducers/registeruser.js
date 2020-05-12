193d2d9ca5a494cb06b36defbc67e181b8d5660f
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-12 12:28:00 -0500

    eliminate console.log's in RegisterUserReducer

--- src/reducers/registeruser.js
+++ src/reducers/registeruser.js
@@ -1,6 +1,4 @@
 const RegisterUserReducer = (state={},action) => {
-  console.log('state',state)
-  console.log('action',action)
   switch(action.type){
     case 'registeruser':
       return Object.assign({},state,{userObj: action.userObj})
