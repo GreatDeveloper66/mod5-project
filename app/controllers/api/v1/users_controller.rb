26e455c5be96f44422e50918bb805af248d11e1d
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-11 19:30:26 -0500

    Add index route to show all users id and username

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+      # @user = User.create(user_params)
+      # if(@user.valid?)
+      #   render json: {user: 'user created', useris: UserSerializer.new(@user)}
+      # else
+      #   render json: {error: 'user creation failed'}
+      # end
+   end
+
+  def show
+  end
+
+  # private
+  #   def user_params
+  #     params.require(:user).permit(:email, :username, :password)
+  #   end
+
 end
