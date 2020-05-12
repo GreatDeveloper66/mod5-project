54e0fe1f578b4555ac8b7553af6c3a500a55880a
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-11 19:19:04 -0500

    Add api/v1/users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
