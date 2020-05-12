e724d844d5cf0a5530c802ed4348ed32c5f377d3
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-12 12:21:02 -0500

    Add home container for login and register screens

---
+++ src/Containers/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css';
+class Home extends Component {
+  constructor(props){
+    super()
+  }
+  render(){
+    return(<div>Home Page</div>)
+  }
+}
+
+export default Home
