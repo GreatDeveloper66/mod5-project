78de613bd2b37fa3977a519d54bc134ddbf85982
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-11 20:15:41 -0500

    change cors header to '*' in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -25,14 +25,14 @@ module Mod5ProjectBackend
     config.load_defaults 6.0
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'example.com'
+    allow do
+      origins '*'
 
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
+  end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
