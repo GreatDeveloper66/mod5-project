9cc1e8befdbe381fcecd4e6094c197782bd3be27
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 14:17:22 -0500

    Add createsequence method to Sequence model

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -2,4 +2,19 @@ class Sequence < ApplicationRecord
   belongs_to :user
   has_many :asanasequences
   has_many :asanas, through: :asanasequences
+  def self.createsequence(name,asanarray)
+    total = self.totalduration(asanarray)
+
+    newsequence = self.create({:name => name, :duration => total})
+    sequenceid = newsequence.id
+
+    asanarray.each_with_index do |asana, index|
+      Asanasequence.create({:asana_id => asana.id,
+                            :sequence_id => sequenceid,
+                            :order => index})
+    end
+  end
+  def self.totalduration(asanarray)
+    asanarray.reduce(0) { |sum,asana| sum + asana.duration }
+  end
 end
