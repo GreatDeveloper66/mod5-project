c2c182d91639e51039256ea8ae942c21c6394388
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 14:27:30 -0500

    Add findasanas method to filter out and find asanasequences for a sequence

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -20,8 +20,11 @@ class Sequence < ApplicationRecord
                           :sequence_id => self.id,
                           :order => self.length})
   end
+  def findasanasequences
+    Asanasequence.all.select { |asequence| asequence.sequence_id === self.id}
+  end
   def self.totalduration(asanarray)
     asanarray.reduce(0) { |sum,asana| sum + asana.duration }
   end
-  
+
 end
