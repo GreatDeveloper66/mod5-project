ede7447d022450639eec4834e6734254f0007331
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 14:01:08 -0500

    Add order column to asanasequences

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_182216) do
+ActiveRecord::Schema.define(version: 2020_05_14_185039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_182216) do
     t.integer "sequence_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "order"
   end
 
   create_table "categories", force: :cascade do |t|
