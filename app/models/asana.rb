7f6e63612e54cc75f2b22013993d0af059757203
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 13:34:21 -0500

    create creatasana method for Asana model

--- app/models/asana.rb
+++ app/models/asana.rb
@@ -3,4 +3,11 @@ class Asana < ApplicationRecord
   has_many :sequences, through: :asanasequences
   has_many :asanacategories
   has_many :categories, through: :asanacategories
+
+  def self.createasana(asanarray)
+    self.create({:sanskritname => asanarray[0],
+                  :englishname => asanarray[1],
+                  :picurl => asanarray[2],
+                  :duration => asanarray[3]})
+  end
 end
