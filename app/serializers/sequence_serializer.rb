6d984a11fb4729874fda363394129042fd80ae5e
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 17:39:24 -0500

    Add asanas sequence to sequence serializer

--- app/serializers/sequence_serializer.rb
+++ app/serializers/sequence_serializer.rb
@@ -1,3 +1,12 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :asanas
+  def asanas
+    self.object.asanas.map do |asana|
+      {
+        englishname: asana.englishname,
+        sanskritname: asana.sanskritname,
+        picurl: asana.picurl
+      }
+    end
+  end
 end
