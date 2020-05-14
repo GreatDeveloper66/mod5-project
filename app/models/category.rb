4a290b66b78b7f9df655dff3e8917be6ef373192
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-14 13:44:23 -0500

    Add createcategory method to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   has_many :asanacategories
   has_many :asanas, through: :asanacategories
+
+  def self.createcategory(name,asanarray)
+    self.create({:name => name})
+    asanarray.each do |asana|
+      Asanacategory.create({:category_id => self.id,
+                             :asana_id => asana.id})
+    end
+  end
 end
