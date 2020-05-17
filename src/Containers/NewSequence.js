1b733b3d1ce06bece3f88bc716725a1d9d3d3ef7
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-17 17:19:35 -0500

    Add carousel component

--- src/Containers/NewSequence.js
+++ src/Containers/NewSequence.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux'
+import AsanaCarousel from '../Components/AsanaCarousel'
 
 class NewSequence extends Component {
 	constructor(props){
 		super()
 	}
 	render(){
-		return('hello')
+		return(
+			<AsanaCarousel />
+		)
 	}
 }
 
