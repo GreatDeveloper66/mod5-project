c506218da2a6cbaf5a942440980357008b8cc62c
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-17 13:50:35 -0500

    Add logic to sequences index and show routes

--- app/controllers/api/v1/sequences_controller.rb
+++ app/controllers/api/v1/sequences_controller.rb
@@ -5,6 +5,18 @@ end
 def destroy
 end
 def index
+	render json: current_user.sequences
+	
 end
 def show
+id = params[:id]
+	@sequence = Sequence.find_by(id: params[:id])
+	 render json: { sequence: SequenceSerializer.new(@sequence) }
 end
+
+
+ private
+
+    def sequence_params
+      params.require(:sequence).permit(:name, :duration )
+    end
