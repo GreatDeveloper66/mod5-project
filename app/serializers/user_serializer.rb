aa7da7e0495192aaa6b55d09511fe37b25538d08
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-17 12:14:24 -0500

    Add field to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username
+  def sequences
+	self.object.sequences.map do |sequence|
+		name: sequence.name,
+		
+	end
+  end
 end
