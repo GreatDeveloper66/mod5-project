e949cedd5620f65d6c2018ec781e69285293d140
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-18 08:23:54 -0500

    Create Saved Sequences Component

---
+++ src/Components/SavedSequences.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { connect } from 'react-redux'
+import { Container, Row, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap'
+
+
+class SavedSequences extends Component {
+	constructor(props){
+		super()
+	}
+	
+	handleEdit = event => {
+		event.preventDefault()
+		console.log('save')
+	}
+	
+	handleDelete = event => {
+		event.preventDefault()
+		
+	}
+
+	render(){
+		return(
+			<Container className="mt-3">
+				<Row className="d-flex justify-content-center">
+					<h2 className="text-primary">SAVED SEQUENCES</h2>
+				</Row>
+				
+				<Row className="d-flex justify-content-center">
+					<Form onSubmit={this.handleEdit}>
+						<FormGroup row>
+							<Col sm={7}>
+								<Label for="exampleSelect" sm={2}>Select</Label>
+									<Input type="select" name="select" id="exampleSelect">
+										<option>Saved Sequence 1</option>
+										<option>Saved Sequence 2</option>
+										<option>Saved Sequence 3</option>
+										<option>Saved Sequence 4</option>
+										<option>Saved Sequence 5</option>
+									</Input>
+       
+							</Col>
+							<Col sm={3}>
+								<Button color="primary" type="submit">EDIT</Button>
+							</Col>
+							<Col sm={3}>
+								<Button color="primary" onClick={this.handleDelete}>DELETE</Button>
+							</Col>
+						</FormGroup>
+					</Form>
+					
+					
+				</Row>
+		</Container>
+		)
+	}
+}
+
+export default FooterBar
