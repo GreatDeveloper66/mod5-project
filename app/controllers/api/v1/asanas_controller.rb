30918e6b1283654c83e2a6ea283116163e0ba0af
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-18 13:30:29 -0500

    Wrap controllers with API class

--- app/controllers/api/v1/asanas_controller.rb
+++ app/controllers/api/v1/asanas_controller.rb
@@ -1,7 +1,10 @@
-def index
-  @asana = Asana.find_by(id: params[:id])
-  render json:@asana
-end
-def show
-  render json: Asana.all
+class Api::V1::AsanasController < ApplicationController
+skip_before_action :authorized, only: [:index,:show]
+	def index
+		render json: Asana.all
+	end
+	def show
+		@asana = Asana.find_by(id: params[:id])
+		render json:@asana
+	end
 end
--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,7 +1,10 @@
-def index
-  render json: Category.all
-end
-def show
-  @category = Category.find_by(id: params[:id])
-  render json: @category
+class Api::V1::CategoriesController < ApplicationController
+skip_before_action :authorized, only: [:index,:show]
+	def index
+		render json: Category.all
+	end
+	def show
+		@category = Category.find_by(id: params[:id])
+		render json: @category
+	end
 end
