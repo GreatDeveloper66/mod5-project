9be1e30846482a777da09a418e1aca113c62c32b
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-19 09:12:40 -0500

    Add add sequence action branch to UserSequences reducer

--- src/reducers/usersequencesreducer.js
+++ src/reducers/usersequencesreducer.js
@@ -2,6 +2,8 @@ const UserSequencesReducer = (state=[],action) => {
   switch(action.type){
     case 'loadsequences':
       return action.sequences
+	case 'addsequence':
+	  return [...state,action.sequence]
     default:
       return state
   }
