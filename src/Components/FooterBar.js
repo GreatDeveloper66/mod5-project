1ec481dc7e289fa437573bc32e871a298e6ab55e
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-19 09:48:53 -0500

    Add state and handleChange method for name input

--- src/Components/FooterBar.js
+++ src/Components/FooterBar.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { Container, Row, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap'
+/*import AddSequenceAction from '../actions/addsequence'*/
+
+/*
+const mapStateToProps = state => {
+	return {
+		sequence: state.sequence
+	}
+}
 
 const mapDispatchToProps = dispatch => {
 	return {
-		
+		addsequence: sequence => {
+			dispatch(AddSequenceAction(sequence))
+		}
 	}
 }
+*/
+
 class FooterBar extends Component {
 	constructor(props){
 		super()
+		this.state = {
+			inputvalue: ''
+		}
+	}
+	
+	handleChange = event => {
+		event.preventDefault()
+		this.setState({inputvalue: event.target.value})
 	}
 	
 	handleSave = event => {
 		event.preventDefault()
+		
+		
 		console.log('save')
 		
 	}
@@ -28,7 +50,7 @@ class FooterBar extends Component {
 								<Label for="name" sm={2}><p className="text-success">NAME:</p></Label>
 							</Col>
 							<Col sm={7}>
-								<Input type="text" name="name" id="name" />
+								<Input type="text" name="name" id="name" value={this.state.inputvalue} onChange={this.handleChange}/>
 							</Col>
 							<Col sm={3}>
 								<Button color="primary" type="submit">SAVE</Button>
