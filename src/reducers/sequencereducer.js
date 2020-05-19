80c8b2a51ad30f6ea946a0e6a419a151408e49ed
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-18 20:26:09 -0500

    Add sequence reducer

--- src/reducers/sequencereducer.js
+++ src/reducers/sequencereducer.js
@@ -1,2 +1,11 @@
+const SequenceReducer = (state=[],action) => {
+  switch(action.type){
+    case 'loadsequence':
+      return action.sequence
+    default:
+      return state
+  }
+}
 
+export default SequenceReducer
 
