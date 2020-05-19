60182592645381e38d05f2bad38c2e641b21f7a6
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-19 14:15:40 -0500

    usersequencesreducer can now add or edit usersequences for new or edited sequence

--- src/reducers/usersequencesreducer.js
+++ src/reducers/usersequencesreducer.js
@@ -4,10 +4,9 @@ const UserSequencesReducer = (state=[],action) => {
       return action.sequences
 	case 'addsequence':
 	  const name = action.sequence.name
-	  const asanas = action.sequence.asanas
 	  const foundsequence = state.find(sequence => sequence.name === name)
 	  if(foundsequence){
-		  return [...state, {id:foundsequence.id,name:name,asanas: asanas}]
+		  return [...state.filter(sequence => sequence.id !== foundsequence.id), {id:foundsequence.id,name:name,asanas: action.sequence.asanas}]
 	  }
 	  else {
 		  action.sequence.id = 101+ state.length
