c26a50a32f9301252bd8d2bf798ae6819d84f845
ashaffermyers <andrewshaffer1986@gmail.com>
2020-05-19 07:46:14 -0500

    Add UserSequencesReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,8 +4,9 @@ import RenderProfileReducer from './renderprofile'
 import LogInMessageReducer from './loginmessagereducer'
 import LoadCategoriesReducer from './loadcategories'
 import SequenceReducer from './sequencereducer'
+import UserSequencesReducer from './usersequencesreducer'
 
-const RootReducer = combineReducers({ jwt:RegisterUserReducer, profile: RenderProfileReducer, loginmessage: LogInMessageReducer,categories: LoadCategoriesReducer, sequence: SequenceReducer })
+const RootReducer = combineReducers({ jwt:RegisterUserReducer, profile: RenderProfileReducer, loginmessage: LogInMessageReducer,categories: LoadCategoriesReducer, sequence: SequenceReducer,sequences: UserSequencesReducer })
 
 export default RootReducer
 
