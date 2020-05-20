f5ddee4988703136a1ec3fab725bbf35869c2a84
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-20 07:19:15 -0500

    Create FooterBarEdit for Edit screen

---
+++ src/Components/FooterBarEdit.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Container, Row, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap'
+import AddSequenceAction from '../actions/addsequence'
+import UndoAsanaAction from '../actions/undoasanas'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => {
+	return {
+		sequence: state.sequence
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		addsequence: sequence => {
+			dispatch(AddSequenceAction(sequence))
+		},
+		undoasana: asana => {
+			dispatch(UndoAsanaAction(asana))
+		}
+	}
+}
+
+
+class FooterBarEdit extends Component {
+	constructor(props){
+		super()
+	}
+	
+	handleSave = event => {
+		event.preventDefault()
+		this.props.addsequence({id:null, name: this.props.sequence.name, asanas: this.props.sequence})
+		
+	}
+
+	render(){
+		return(
+			<Container className="mt-3">
+				<Row className="d-flex justify-content-end">
+					<Form onSubmit={this.handleSave}>
+						<FormGroup row>
+							<Col sm={1}>
+								<Button color="danger" onClick={() => this.props.undoasana()}>X</Button>
+							</Col>
+							<Col sm={2}>
+								<Label for="name" sm={2}><p className="text-success">NAME:</p></Label>
+							</Col>
+							<Col sm={6}>
+								{this.props.sequence.name}
+							</Col>
+							<Col sm={3}>
+								<Button color="primary" type="submit">SAVE</Button>
+							</Col>
+						</FormGroup>
+					</Form>
+					
+					
+				</Row>
+		</Container>
+		)
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(FooterBarEdit)
