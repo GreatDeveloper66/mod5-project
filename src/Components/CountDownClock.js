6a243a2e3cb1e03bcfad0ecb9b0845cdc721c37d
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-21 18:27:44 -0500

    Clock at the right now cycles through durations

--- src/Components/CountDownClock.js
+++ src/Components/CountDownClock.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import ReactCountdownClock from "react-countdown-clock";
 import { Container, Row, Col,Card } from 'reactstrap'
 import { connect } from 'react-redux'
- 
+import IncSlideAction from '../actions/incslide'
+
+const mapDispatchToProps = dispatch => {
+	return {
+		incslide:() => dispatch(IncSlideAction())
+	}
+}
+
  
 const mapStateToProps = state => {
 		return {
-			sequence: state.sequence
+			sequence: state.sequence,
+			slide: state.slide
 		}
 	} 
  
@@ -14,10 +22,17 @@ class CountDownClock extends Component {
   constructor(props){
 	super()
   }
+  
+ //map state slide to component
+ //when done increment counter
+ //get new duration
+ //for other component render asana based on slide number
  
- findDuration = () => {
-		return this.props.sequence.asanas.reduce((sum,asana) => sum + asana.duration,0)*60
-	}
+ 
+findDuration = () => this.props.sequence.asanas.reduce((sum,asana) => sum + asana.duration,0)*60
+
+findInterval = () => this.props.sequence.asanas[this.props.slide].duration * 60
+
 	
   render() {
     const settings = {
@@ -38,7 +53,7 @@ class CountDownClock extends Component {
                      color="#444"
                      alpha={0.9}
                      size={150}
-                     onComplete={this.timeUp}
+                     onComplete={() => console.log('complete')}
 					 timeFormat="hms"/>
 			</Card>
 		</Col>
@@ -51,11 +66,11 @@ class CountDownClock extends Component {
 		</Col>
 		<Col xs={3} className="d-flex justify-content-center align-items-center">
 			<Card>
-			<ReactCountdownClock seconds={60}
+			<ReactCountdownClock key={this.props.slide} seconds={this.findInterval()}
                      color="#444"
                      alpha={0.9}
                      size={100}
-                     onComplete={this.timeUp}
+                     onComplete={() => this.props.incslide()}
 					 timeFormat="hms"/>
 		</Card>
 		</Col>
@@ -65,4 +80,4 @@ class CountDownClock extends Component {
   }
 }
 
-export default connect(mapStateToProps,null)(CountDownClock)
+export default connect(mapStateToProps,mapDispatchToProps)(CountDownClock)
