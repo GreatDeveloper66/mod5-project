7c178894021d1bece5739a5e05f358b33a542b56
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-21 17:50:35 -0500

    Connect inc slider state and action to CountDown Clock

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,8 +5,11 @@ import LogInMessageReducer from './loginmessagereducer'
 import LoadCategoriesReducer from './loadcategories'
 import SequenceReducer from './sequencereducer'
 import UserSequencesReducer from './usersequencesreducer'
+import SlideReducer from './slidereducer'
 
-const RootReducer = combineReducers({ jwt:RegisterUserReducer, profile: RenderProfileReducer, loginmessage: LogInMessageReducer,categories: LoadCategoriesReducer, sequence: SequenceReducer,sequences: UserSequencesReducer })
+const RootReducer = combineReducers({ jwt:RegisterUserReducer, profile: RenderProfileReducer, 
+									loginmessage: LogInMessageReducer,categories: LoadCategoriesReducer, 
+									sequence: SequenceReducer,sequences: UserSequencesReducer, slide: SlideReducer })
 
 export default RootReducer
 
