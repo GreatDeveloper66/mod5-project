b158d363d0ce2131ec189867860a410b332a706a
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-22 16:55:54 -0500

    change styling to pics and text in YogaWorkout screen

--- src/Containers/YogaWorkoutScreen.js
+++ src/Containers/YogaWorkoutScreen.js
@@ -5,25 +5,35 @@ import { connect } from 'react-redux'
 import { Container, Row, Col, Card,CardBody,CardTitle,CardText,Jumbotron } from 'reactstrap'
 import Beginner_Sequence from '../images/Beginner_Sequence.jpg'
 
+const bannerStyle = {
+		backgroundImage: `url(${require('../images/Beginner_Sequence.jpg')})`,
+		backgroundSize: 'contain',
+		backgroundPosition: 'center',
+		backgroundRepeat: 'no-repeat',
+		backgroundAttachment: 'fixed',
+		height: '500px',
+		width: '100%'
+	}
 
 class YogaWorkoutScreen extends Component {
 	constructor(props){
 		super()
 	}
+	
+	
 	render(){
 		return(
-		<div style={{backgroundImage: `url(${require('../images/Beginner_Sequence.jpg')})`}}>
-			asfsdaf
-				Overlayed Text.
-				<br />
-				afsafa
-				<br />
-				afsafas
-				<br />
-				afsdfasdfa
-				afsdfasdfsa
-				afsdafas
-				afdsaf
+		<div style={bannerStyle} className="d-flex justify-content-center align-items-center">
+			<Container>
+				<Row className="d-flex justify-content-center align-items-center">
+			<h1>YOGA BEGINNER</h1>
+			</Row>
+			
+			<Row>
+			<h2>60 minutes | Level 1</h2>
+			</Row>
+			</Container>		
+			
 				{/*<Card>
 					<CardBody>
 						<CardTitle>
