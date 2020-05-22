69fe5b7045f25d3ad779d3972e00da7c7c1a33dd
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-22 13:37:51 -0500

    Create Yoga Workout Screnn Component

---
+++ src/Containers/YogaWorkoutScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+import AsanaCard from './AsanaCard'
+import { connect } from 'react-redux'
+import { Container, Row, Col, Card,CardBody } from 'reactstrap'
+
+class YogaWorkoutsScreen extends Component {
+	constructor(props){
+		super()
+	}
+	render(){
+		return(
+			<div></div>
+		)
+	}
+}
