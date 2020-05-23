fdfac5807d04019e12f0a1f821c95d94d0b1d527
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-23 14:45:24 -0500

    Eliminate comments form file

--- src/Containers/YogaWorkoutScreen.js
+++ src/Containers/YogaWorkoutScreen.js
@@ -5,9 +5,6 @@ import { connect } from 'react-redux'
 import { Container, Row, Col, Card,CardBody,CardTitle,CardText,Jumbotron } from 'reactstrap'
 import Beginner_Sequence from '../images/Beginner_Sequence.jpg'
 
-
-
-
 const bannerStyle = {
 		backgroundImage: `url(${require('../images/Beginner_Sequence.jpg')})`,
 		backgroundSize: 'contain',
@@ -29,28 +26,12 @@ class YogaWorkoutScreen extends Component {
 		<div style={bannerStyle} className="d-flex justify-content-center align-items-center">
 			<Container>
 				<Row className="d-flex justify-content-center align-items-center">
-			<h1>YOGA BEGINNER</h1>
-			</Row>
-			
+					<h1>YOGA BEGINNER</h1>
+				</Row>			
 			<Row className="d-flex justify-content-center align-items-center">
 			<h2>60 minutes | Level 1</h2>
 			</Row>
 			</Container>		
-			
-				{/*<Card>
-					<CardBody>
-						<CardTitle>
-							Beginner's Sequence
-						</CardTitle>
-					</CardBody>
-					<img src={require(`../images/Beginner_Sequence.jpg`)} alt="Beginner Sequence pic" />
-					<CardBody>
-						<CardText>
-							60 minutes | Level 1
-						</CardText>
-					</CardBody>
-				</Card>
-				*/}
 			
 		</div>
 		)
