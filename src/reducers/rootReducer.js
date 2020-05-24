9219e43a9211ee555b7d3fa8e9e2ca786e6df2fc
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-24 18:28:39 -0500

    Add background picture to home page

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import CategoryReducer from './categoryreducer'
 import RegisterUserReducer from './registeruser'
 import RenderProfileReducer from './renderprofile'
 import LogInMessageReducer from './loginmessagereducer'
@@ -7,9 +8,13 @@ import SequenceReducer from './sequencereducer'
 import UserSequencesReducer from './usersequencesreducer'
 import SlideReducer from './slidereducer'
 
+
 const RootReducer = combineReducers({ jwt:RegisterUserReducer, profile: RenderProfileReducer, 
 									loginmessage: LogInMessageReducer,categories: LoadCategoriesReducer, 
-									sequence: SequenceReducer,sequences: UserSequencesReducer, slide: SlideReducer })
+									sequence: SequenceReducer,sequences: UserSequencesReducer, slide: SlideReducer,
+									category: CategoryReducer })
+									
+console.log(RootReducer)
 
 export default RootReducer
 
