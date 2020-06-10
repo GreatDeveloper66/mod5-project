f8cf223652f3f6e368d6d8ac72eb4ac58c9c0e54
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-10 14:29:58 -0500

    Add loading categories option to Categories Reducer

--- mod5-project-frontend/src/reducers/loadcategories.js
+++ mod5-project-frontend/src/reducers/loadcategories.js
@@ -2,6 +2,8 @@ const CategoriesReducer = (state={categories: [], requesting: false},action) => 
   switch(action.type){
     case 'loadcategories':
       return {categories: action.categories, requesting: false}
+    case 'loadingcategories':
+      return {categories: [], requesting: true}
     default:
       return state
   }
