b06970f7102a6b92fc83a472fa1955b07ab07ae3
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-03-09 20:29:02 -0600

    Active record schema update

--- mod5-project-backend/db/schema.rb
+++ mod5-project-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_153848) do
+ActiveRecord::Schema.define(version: 2020_06_20_010948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,27 @@ ActiveRecord::Schema.define(version: 2020_05_22_153848) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "counterasanas", force: :cascade do |t|
+    t.integer "counter_id"
+    t.integer "countee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "openingasanas", force: :cascade do |t|
+    t.integer "opener_id"
+    t.integer "openee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "preparationasanas", force: :cascade do |t|
+    t.integer "preparer_id"
+    t.integer "preparee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "sequences", force: :cascade do |t|
     t.string "name"
     t.integer "duration"
@@ -53,6 +74,13 @@ ActiveRecord::Schema.define(version: 2020_05_22_153848) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_sequences_on_user_id"
+  end
+
+  create_table "stabalizingasanas", force: :cascade do |t|
+    t.integer "stabalizer_id"
+    t.integer "stabalizee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
